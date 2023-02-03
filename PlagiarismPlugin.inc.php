c1d020eac4878bc635cc6028c5c8ecf37b700775
Clinton Graham <ctgraham@pitt.edu>
2023-02-03 16:14:19 -0500

    #23, #28: find group id before creating folder

--- PlagiarismPlugin.inc.php
+++ PlagiarismPlugin.inc.php
@@ -176,6 +176,8 @@ class PlagiarismPlugin extends GenericPlugin {
 		$ithenticate = null;
 		try {
 			$ithenticate = $this->ithenticateConnect($username, $password, $contextName);
+			$groupList = $ithenticate->fetchGroupList();
+			$groupId = array_search($contextName, $groupList);
 		} catch (Exception $e) {
 			$this->sendErrorMessage($submission->getId(), $e->getMessage());
 			return false;
